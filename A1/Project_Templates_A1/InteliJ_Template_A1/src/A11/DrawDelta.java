0135559ac013feded8ff35b3f767f6d751aa408e
andreas <andreasth005@gmail.com>
2024-10-06 18:43:52 +0300

    added input check

--- A1/Project_Templates_A1/InteliJ_Template_A1/src/A11/DrawDelta.java
+++ A1/Project_Templates_A1/InteliJ_Template_A1/src/A11/DrawDelta.java
@@ -11,7 +11,28 @@ import java.awt.geom.Line2D;
 import java.io.PrintWriter;
 
 public class DrawDelta {
+
+    static void inputCheck(String[] consoleArguments)
+    {
+        String M;
+        int L;
+        try{
+            M = consoleArguments[0];
+            L = Integer.parseInt(consoleArguments[1]);
+
+            if (!(M.equals("c") || M.equals("w") || M.equals("f") || M.equals("g"))) {throw new Exception();}
+            if (L < 3 || L > 20) {throw new Exception();}
+            if (M.length() > 1) {throw new Exception();}
+        }
+        catch(Exception e){
+
+            System.out.println("Error. Please provide the correct inputs.");
+            return;
+        }
+    }
     public static void main(String[] args) {
         // Main method to handle input parameters and call appropriate methods based on mode (M)
+        inputCheck(args);
+
     }
 }
