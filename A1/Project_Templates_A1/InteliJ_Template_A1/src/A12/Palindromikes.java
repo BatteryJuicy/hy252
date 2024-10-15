f44ae3e222dd94daf0cf07a12d5ad7c642b479af
andreas <andreasth005@gmail.com>
2024-10-15 22:00:46 +0300

    started A2

--- A1/Project_Templates_A1/InteliJ_Template_A1/src/A12/Palindromikes.java
+++ A1/Project_Templates_A1/InteliJ_Template_A1/src/A12/Palindromikes.java
@@ -2,12 +2,49 @@ package A12;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.InvalidParameterException;
 import java.text.Normalizer;
+import java.util.regex.Pattern;
 import java.util.Scanner;
 
 public class Palindromikes {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // Main method to manage the selection and execution of different functionalities
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a palindrome: ");
+        String palindrome = in.nextLine();
+        System.out.println("\n" + isPalindromikiFrash(palindrome));
+
+    }
+
+    static boolean isPalindromikiFrash(String s)
+    {
+        s = Normalizer.normalize(s, Normalizer.Form.NFD);
+        //System.out.println("string after normalize: " + s);
+        s = Pattern.compile("\\p{M}").matcher(s).replaceAll("");
+        //System.out.println("string after replacing accents: " + s);
+        s = s.replaceAll("[^\\p{L}]", "");
+        //System.out.println("string after removing special characters: " + s);
+        s = s.toLowerCase();
+        //System.out.println("string after replacing to lowercase: " + s);
+
+        char[] normal = s.toCharArray();
+        char[] reverse = new char[s.length()];
+        boolean isPalindrome = true;
+
+        if (s.isEmpty())
+            throw new InvalidParameterException();
+
+        for (int i = s.length() - 1; i >= 0; i--)
+        {
+            reverse[s.length() - 1 - i] = normal[i];
+        }
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (normal[i] != reverse[i]){isPalindrome = false;}
+        }
+        return isPalindrome;
     }
 }
 
