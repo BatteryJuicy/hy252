c8851578fd54c389cdde3f071f37f4fb5b24185f
andre <andreasth005@gmail.com>
2024-10-20 22:15:24 +0300

    A13a done

--- A1/Project_Templates_A1/InteliJ_Template_A1/src/A13/Sudoku.java
+++ A1/Project_Templates_A1/InteliJ_Template_A1/src/A13/Sudoku.java
@@ -24,6 +24,37 @@ class SudokuValidator {
     // (α) Method to check if the given board is valid according to Sudoku rules
     public static boolean isValidBoard(int[][] board) {
 
+        boolean[] invRow = new boolean[board.length];
+        boolean[] invCol = new boolean[board.length];
+        boolean[] invBox = new boolean[board.length];
+
+        for (int i = 0; i < board.length; i++) {
+            Arrays.fill(invRow, false);
+            Arrays.fill(invCol, false);
+            Arrays.fill(invBox, false);
+
+            for (int j = 0; j < board[0].length; j++) {
+
+                //valid cell value check.
+                if (board[i][j] > 9 || board[i][j] < 0) return false;
+
+                //ignore 0 to not get nullPointerException below.
+                if(board[i][j] == 0) {continue;}
+
+                //checking if the number of the cell that corresponds to the appropriate inv-array index is true.
+                //if it's true it means it has already been "seen" before so there is a duplicate in the row or column.
+                if (invRow[board[i][j] - 1] || invCol[board[j][i] - 1]) return false;
+                else{
+                    invRow[board[i][j] - 1] = true;
+                    invCol[board[j][i] - 1] = true;
+                }
+
+                if(invBox[board[i%3 + 3*(j%3)][i/3 + 3*(j/3)] - 1])return false;
+                else{
+                    invBox[board[i][j] - 1] = true;
+                }
+            }
+        }
         return false;
     }
 
